f3a5e2e11bc27e96c766d7e8fba435cdd63e8628
camilaarce <camilarce2710@gmail.com>
2023-08-16 18:27:21 -0300

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,30 +1,44 @@
 <template>
-    <v-footer class="d-flex flex-column mt-10" color="black">
-        <div class="d-flex w-100 align-center px-4 justify-center">
-            <v-btn v-for="icon in icons" :key="icon" class="mx-4 icons" :icon="icon" variant="plain" size="small"></v-btn>
-        </div>
+  <v-footer class="d-flex flex-column mt-10" color="black">
+    <div class="d-flex w-100 align-center px-4 justify-center">
+      <a :href="red.link" v-for="red in redes" :key="red.id"
+        ><v-btn
+          :icon="red.icon"
+          class="mx-2 icons"
+          variant="text"
+          size="large"
+          onclick=""
+        ></v-btn
+      ></a>
+    </div>
 
-        <div class="text-center my-5">
-            {{ new Date().getFullYear() }} — <strong>Camila Arce</strong>
-        </div>
-    </v-footer>
+    <div class="text-center my-5">
+      {{ new Date().getFullYear() }} — <strong>Camila Arce</strong>
+    </div>
+  </v-footer>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        icons: [
-            'mdi-facebook',
-            'mdi-twitter',
-            'mdi-linkedin',
-            'mdi-instagram',
-        ],
-    }),
-}
+  data: () => ({
+    redes: [
+      {
+        id: 0,
+        icon: "mdi-github",
+        link: "https://github.com/camilaarce",
+      },
+      {
+        id: 1,
+        icon: "mdi-linkedin",
+        link: "https://www.linkedin.com/in/camila-arce-96107b1b9/",
+      },
+    ],
+  }),
+};
 </script>
 
 <style scoped>
-    .icons {
-        color: rgb(255, 117, 221);
-    }
+.icons {
+  color: rgb(255, 117, 221);
+}
 </style>
